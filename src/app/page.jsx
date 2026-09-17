6579c4b0d4f1a3c5128a59fa337760b0781ce02a
BIO62 <BIO62@users.noreply.github.com>
2026-09-17 15:30:24 +0800

    style(hero): remove italic from Philosopher hero heading

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -29,7 +29,7 @@ function Clients() {
       <Container>
         <FadeIn className="flex items-center gap-x-8">
           <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
-            Бидний албан ёсны дистрибьютерээр төлөөлж буй олон улсын брэндүүд
+            Бидний албан ёсны дистрибьютерээр олон улсын брэндүүд
           </h2>
           <div className="h-px flex-auto bg-neutral-800" />
         </FadeIn>
